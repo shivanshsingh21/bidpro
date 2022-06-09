dc87c0aa27d86673a7f12d1715cdeac5f6850884
SHIVANSh <interra.shivansh@gmail.com>
2022-06-09 10:01:43 +0530

    41512

--- frontend/src/component/molecules/index.tsx
+++ frontend/src/component/molecules/index.tsx
@@ -3,8 +3,7 @@ import MenuBar from "./Menubar.organism"
 import MainBar from "./MainBar.organism";
 import Footer from "./Footer.organism";
 import LoginFrom from "./LoginForm/LoginForm.organism"
-import RegistrationForm from "./RegistrationForm/RegistrationForm.organism";
 
 module.exports={
-    NavBar, MenuBar,MainBar,Footer,LoginFrom, RegistrationForm
+    NavBar, MenuBar,MainBar,Footer,LoginFrom,
 }
